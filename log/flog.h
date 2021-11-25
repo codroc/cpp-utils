6dec3e52f4a6f7f4914bf8bd10387476f27b1f90
codroc <codroc@163.com>
2021-11-25 09:32:28 +0800

    dem

--- log/flog.h
+++ log/flog.h
@@ -1,3 +1,6 @@
+#ifndef FLOG_H
+#define FLOG_H
+
 #include "logger.h"
 #include "logStream.h"
 #include "asyncLogging.h"
@@ -18,3 +21,5 @@ private:
     std::unique_ptr<AsyncLoggingWatcher> _watcher;
     std::unique_ptr<LoggerWatcher> _lw; // 用于退出程序时回收 Logger 中的资源，同时达到刷新 缓冲区 的作用
 };
+
+#endif
