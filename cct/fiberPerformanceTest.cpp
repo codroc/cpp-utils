ac510fa1b34ebf36c2ba34c3352bf1aa5e78973e
codroc <codroc@163.com>
2022-02-26 14:05:06 +0800

    add thread swap performance test in fiberPerformance.cpp

--- cct/fiberPerformanceTest.cpp
+++ cct/fiberPerformanceTest.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "fiber.h"
+#include "thread.h"
+#include "mutex.h"
+#include "condition.h"
 #include "timestamp.h"
 
 int i = 0;
@@ -22,12 +25,68 @@ void performanceTest() {
     }
 }
 
-int main(int argc, char** argv) {
+void testFiberSwapPerformance() {
     Fiber::GetThis();
     Fiber::ptr test(new Fiber(performanceTest, 4096));
     t1 = Timestamp::now();
     while(i < kTimes) {
         test->swapIn();
     }
+}
+
+struct PC {
+    PC()
+        : lock(),
+          c(lock)
+    {}
+    void Produce() {
+        MutexGuard guard(lock);
+        if(i == 0) {
+            i++;
+            c.wakeup();
+        }
+        else
+            c.wait();
+    }
+    void Consume() {
+        MutexGuard guard(lock);
+        if(i == 1) {
+            i--;
+            c.wakeup();
+        }
+        else
+            c.wait();
+    }
+    MutexLock lock;
+    Condition c;
+    int i = 0;
+};
+PC test;
+void Producer() {
+    while(++i< kTimes)
+        test.Produce();
+}
+void Consumer() {
+    while(i < kTimes)
+        test.Consume();
+    int ms;
+    t2 = Timestamp::now();
+    ms = t2 - t1;
+    printf("time use %d\n", ms); 
+}
+
+void testThreadSwapPerformance() {
+    Thread th1(Producer);
+    Thread th2(Consumer);
+    t1 = Timestamp::now();
+    th1.start();
+    th2.start();
+    th1.join();
+    th2.join();
+}
+
+int main(int argc, char** argv) {
+    // testFiberSwapPerformance();
+    testThreadSwapPerformance();
     return 0;
 }
